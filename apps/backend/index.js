ae2595c64c660219011e5bd73a5a2e9b0a95f62e
Peter <pedrojuana08@gmail.com>
2025-10-24 19:36:57 +0800

    initial commit: Full stack turbo repo

---
+++ apps/backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import process from 'node:process';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('port', PORT);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}`);
+});
