1ed12abc772be63915519c096929eb3d4d87a669
Peter <pedrojuana08@gmail.com>
2025-10-27 16:00:30 +0800

    fix validatemiddleware

--- apps/backend/src/modules/middleware/login-middleware.js
+++ apps/backend/src/modules/middleware/login-middleware.js
@@ -1,31 +1,33 @@
 import { User } from '../models/user-schema.js';
+import bcrypt from 'bcryptjs';
 
-export async function ValidateLogin(req, res, next){
-  console.log('Request body', req.body);
-   const { username, password } =req.body;
-
-  try{
-    //validate the username and password
-   if(!username || !password){
-    return res.status(400).json({ message: 'Invalid credentials'});
-   }
-
-     const user = await User.findOne({ username });
-
-    if(!user) {
-      return res.status(400).json({ message: 'User not found'});
+export async function validateLogin(req, res, next) {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: 'No request body provided' });
     }
 
+    const { username, password } = req.body;
 
-   if (user.password !== password){
-    return res.status(401).json({message: 'Wrong credentials'});
-   }
-   req.user = user;
-   next();
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Username and password are required' });
+    }
 
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
 
-  } catch(err){
-    console.error('Error in Fetching data', err);
-    return res.status(500).json({ message: 'Internal server in middleware'});
+    // hasing password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: 'Wrong credentials' });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error('Error in validateLogin middleware:', err);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 }
--- apps/backend/src/modules/routes/routes.js
+++ apps/backend/src/modules/routes/routes.js
@@ -3,11 +3,11 @@ import express from 'express';
 import { register } from '../controller/registration-controller.js';
 import { validateRegister } from '../middleware/registration-middleware.js';
 import { login } from '../controller/login-controller.js';
-import { ValidateLogin } from '../middleware/login-middleware.js';
+import { validateLogin } from '../middleware/login-middleware.js';
 
 const router = express.Router();
 
 router.post('/register', validateRegister, register);
-router.post('/login', ValidateLogin, login);
+router.post('/login', validateLogin, login);
 
 export default router;
