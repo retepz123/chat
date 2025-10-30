b6d1b958f223e95fd69f5a2dc3420e3c5f565236
Peter <pedrojuana08@gmail.com>
2025-10-30 17:17:15 +0800

    added plugins

--- apps/backend/src/modules/utils/cookie.js
+++ apps/backend/src/modules/utils/cookie.js
@@ -2,7 +2,7 @@ export const setTokenCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none', // allow to cross-origin in localhost
+sameSite: isProduction ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
